851c7b17c611d24abe0ecdcc7796c2a03b1ff506
senthilselva1998 <17selvabalu3@gmailcom>
2018-06-01 11:58:44 +0530

    control process

---
+++ control.java
@@ -0,0 +1,14 @@
+package javabasic;
+
+public class controlprogram {
+ public static void main(String[] args) {
+	 int count = 10;
+	 if(count<20) { 
+		 System.out.println(20);
+	 }else if(count == 10) {
+		 System.out.println(10);
+	}else {
+		System.out.println("None");
+	}
+ }
+}
