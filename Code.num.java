d40ca19592f5341b4565ca29694fc1ac806c634c
senthilselva1998 <17selvabalu3@gmailcom>
2018-06-22 17:33:16 +0530

    Mystray

---
+++ Code.num.java
@@ -0,0 +1,15 @@
+public class CodeNum
+{
+public static main (String[] args)
+{
+int n;
+Scanner sc=new scanner(System.in)
+for(1<=N<=100000)
+{
+System.out.println("positive");
+System.out.println("negative");
+
+
+}
+ }
+}
